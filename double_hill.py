a8a3c57127181c21f731fd8061d3e64499282dab
Aditi sahu <aditisahu.1405@gmail.com>
2024-04-27 12:38:37 +0530

    double_hill.py

--- double_hill.py
+++ double_hill.py
@@ -1,7 +1,7 @@
 n=int(input("Enter number of rows:"))
 
 for i in range (n+1):
-    for j in range(2*n+1):
+    for j in range(1,2*n):
         if j<=i or j>=2*n-i:
             print("*",end=" ")
         else:
